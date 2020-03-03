cb76da45964f81615201f386fbdaaec07076e5f5
Hugo Sánchez V <hsanchez@mediomelon.mx>
2020-03-03 03:15:23 -0600

    Agregado sql de inserciones

---
+++ docker/sql/3.inserts.sql
@@ -0,0 +1,48 @@
+INSERT INTO muebleria_db.CAT_ROLES(ID,NOMBRE)
+VALUES  (1,'CLIENTE'),
+        (2,'ADMIN');
+
+INSERT INTO muebleria_db.CAT_TIPO_MUEBLE(ID, NOMBRE)
+VALUES  (1,'SILLON'),
+        (2,'MESA'),
+        (3,'SILLA'),
+        (4,'TABURETE'),
+        (5,'ESCRITORIOS');
+
+INSERT INTO muebleria_db.CAT_ESTILO_MUEBLE(ID,NOMBRE)
+VALUES  (1,'Reproducción'),
+        (2,'Contemporáneos'),
+        (3,'Coloniales'),
+        (4,'Art Noveu'); 
+
+INSERT INTO muebleria_db.CAT_MATERIAL_MUEBLE(ID,NOMBRE)
+VALUES  (1,'Madera'),
+        (2,'Aluminio'),
+        (3,'Metal'),
+        (4,'Fibra'),
+        (5,'Plástico y resina'),
+        (6,'Barro esmaltado'),
+        (7,'Cuero'),
+        (8,'Vinil');
+
+INSERT INTO muebleria_db.CAT_MENSAJERIA(NOMBRE ,TIEMPO, COSTO )
+VALUES  ('DHL',3,90),
+        ('FedEx',4,80),
+        ('UPS',2,130);
+    
+INSERT INTO muebleria_db.TIENDA (RFC ,NOMBRE )
+VALUES ('MUE900201XY3','Muebles S.A de C.V'),
+		('REM911105AB3','Remates S.A'),
+		('CON021202','Confort S.A');
+
+-- Usuario cliente
+INSERT INTO muebleria_db.USUARIO (NOMBRE_USUARIO ,CLAVE_ACCESO ,ID_ROL )
+VALUES ('hsanchez', '1c7a92ae351d4e21ebdfb897508f59d6', 1);
+
+-- Administrador
+INSERT INTO muebleria_db.USUARIO (NOMBRE_USUARIO ,CLAVE_ACCESO ,ID_ROL )
+VALUES ('admin', '1c7a92ae351d4e21ebdfb897508f59d6', 2);
+
+-- Cliente
+INSERT INTO muebleria_db.CLIENTES (NOMBRE ,PRIMER_APELLIDO ,SEGUNDO_APELLIDO ,TEL_OFICINA,EMAIL ,DIRECCION_FISCAL ,ID_USUARIO ,ID_TIENDA )
+VALUES('Daniela', 'Rosales','Lopez', '712345','drosales@mediomelon.mx','Col. Centro, Av 2, Orizaba, Veracruz',2,1);
